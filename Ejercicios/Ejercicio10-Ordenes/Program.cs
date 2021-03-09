e502dc66f330d5bbe021d49d735ff2e424ab48bd
Nelson Chacon <cursodecomputacion.net@gmail.com>
2021-03-09 11:54:53 -0600

    Ejercicio 10

---
+++ Ejercicios/Ejercicio10-Ordenes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicio10_Ordenes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
